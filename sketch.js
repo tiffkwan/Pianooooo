889c71d489220825ac1bea0a3bdbfe4f1d98d5dd
Tiff Kwan <64667727+tiffkwan@users.noreply.github.com>
2020-05-02 23:26:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
  * May 3, 2020
  */
 let notes = [60, 62, 64, 65, 67, 69, 71, 74];
-//let blackkeys = [61, 63, 66, 68, 70];
+let blackkeys = [61, 63, 66, 68, 70];
 let index = 0;
 let trigger = 0;
 let osc;
@@ -19,7 +19,7 @@ function setup() {
 }
 
 // A function to play a note
-function playNote(note, duration) {
+function playNotes(note, duration) {
   osc.freq(midiToFreq(note));
   // Fade it in
   osc.fade(0.5, 0.2);
@@ -46,124 +46,120 @@ function draw() {
   line(7 * width / 8, 0, 7 * width / 8, height);
   line(8 * width / 8, 0, 8 * width / 8, height);
 
-  // Depending on mouse location, a different rectangle is set to a color
-  //C note- white rect 1
+// Colors will depend on location of the mouse
+  //C note- white key 1
   if (mouseX < width / 8 && mouseX > 0) {
     fill(255, 179, 217); // pink
     rect(0, 0, width / 8, height);
 
-    //D note- white rect 2
+    //D note- white key 2
   } else if (mouseX < 2 * width / 8) {
     fill(255, 203, 151); // light orange
     rect(width / 8, 0, width / 8, height);
 
-    //E note- white rect 3
+    //E note- white key 3
   } else if (mouseX < 3 * width / 8) {
     fill(255, 255, 164); // light yellow
     rect(2 * width / 8, 0, width / 8, height);
 
-    //F note- white rect 4
+    //F note- white key 4
   } else if (mouseX < 4 * width / 8) {
     fill(162, 255, 162); // light green
     rect(3 * width / 8, 0, width / 8, height);
 
-    //G note- white rect 5
+    //G note- white key 5
   } else if (mouseX < 5 * width / 8) {
     fill(158, 241, 231); // light aqua
     rect(4 * width / 8, 0, width / 8, height);
 
-    //A note- white rect 6
+    //A note- white key 6
   } else if (mouseX < 6 * width / 8) {
     fill(145, 200, 255); // light blue
     rect(5 * width / 8, 0, width / 8, height);
 
-    //B note- white rect 7
+    //B note- white key 7
   } else if (mouseX < 7 * width / 8) {
     fill(177, 180, 254); // light indigo
     rect(6 * width / 8, 0, width / 8, height);
 
-    //C note- white rect 8
+    //C note- white key 8
   } else if (mouseX < 8 * width / 8) {
     fill(210, 177, 254); // light purple
     rect(7 * width / 8, 0, width / 8, height);
   }
 
 
-  //C# note - black rect 1
+  //C# note - black key 1
   fill(0);
   rect(3 / 4 * width / 8, 0, width / 16, height / 2)
 
   if (mouseX >= 3 / 32 * width && mouseX <= 5 / 32 * width && mouseY < height / 2) {
-    if (mouseIsPressed) {
-      //play music
-      fill(189, 0, 0); //dark red
+    if (mouseIsPressed) { // play music
+      fill(189, 0, 0); // dark red
       rect(3 / 4 * width / 8, 0, width / 16, height / 2);
     } else {
-      fill(0);
+      fill(0); // no music
     }
   }
 
-  //D# note - black rect 2
+  //D# note - black key 2
   fill(0);
   rect(7 / 4 * width / 8, 0, width / 16, height / 2);
   if (mouseX >= 7 / 32 * width && mouseX <= 9 / 32 * width && mouseY < height / 2) {
-    if (mouseIsPressed) {
+    if (mouseIsPressed) { // music plays
       fill(185, 133, 62); //dark orange
       rect(7 / 4 * width / 8, 0, width / 16, height / 2);
     } else {
-      fill(0);
+      fill(0); // silent
     }
   }
 
-  //F# note - black rect 4
+  //F# note - black key 4
   fill(0);
   rect(15 / 4 * width / 8, 0, width / 16, height / 2)
 
   if (mouseX >= 15 / 32 * width && mouseX <= 17 / 32 * width && mouseY < height / 2) {
-    if (mouseIsPressed) {
-      //play music
+    if (mouseIsPressed) {  // play music
       fill(216, 203, 48); // dark yellow
       rect(15 / 4 * width / 8, 0, width / 16, height / 2);
     } else {
-      fill(0);
+      fill(0); // silent
     }
   }
 
-  //G# note - black rect 5
+  //G# note - black key 5
   fill(0);
   rect(19 / 4 * width / 8, 0, width / 16, height / 2);
   if (mouseX >= 19 / 32 * width && mouseX <= 21 / 32 * width && mouseY < height / 2) {
-    if (mouseIsPressed) {
-      //play music
+    if (mouseIsPressed) { //play music
       fill(61, 159, 32); // dark green
       rect(19 / 4 * width / 8, 0, width / 16, height / 2);
     } else {
-      fill(0);
+      fill(0); // silent
     }
   }
 
-  //A# note- black rect 6
+  //A# note- black key 6
   fill(0);
   rect(23 / 4 * width / 8, 0, width / 16, height / 2);
   if (mouseX >= 23 / 32 * width && mouseX <= 25 / 32 * width && mouseY < height / 2) {
-    if (mouseIsPressed) {
-      //play music
+    if (mouseIsPressed) { //play music
       fill(17, 103, 174); // dark blue
       rect(23 / 4 * width / 8, 0, width / 16, height / 2);
     } else {
-      fill(0);
+      fill(0); // play no music
     }
   }
 
-  // C# - black rect 7
+  // C# - black key 7
   fill(0);
   rect(31 / 4 * width / 8, 0, width / 32, height / 2);
   if (mouseX >= 31 / 32 * width && mouseX <= 33 / 32 * width && mouseY < height / 2) {
-    if (mouseIsPressed) {
+    if (mouseIsPressed) { // play
       fill(164, 0, 164); // dark purple
       rect(31 / 4 * width / 8, 0, width / 32, height / 2);
     } else {
-      fill(0);
+      fill(0); // silent
     }
 
   }
@@ -173,7 +169,7 @@ function mousePressed(pplayed) {
   if (pplayed.button == 0 && pplayed.clientX < width && pplayed.clientY < height) {
     // Map mouse to the key index
     let key = floor(map(mouseX, 0, width, 0, notes.length));
-    playNote(notes[key]);
+    playNotes(notes[key]);
   }
 }
 
